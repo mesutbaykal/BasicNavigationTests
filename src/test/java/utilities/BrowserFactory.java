b462c72d4d22fb6425731ea375dc5ef5017e8a1c
mesut.baykal <maxutbykal41@gmail.com>
2022-09-13 11:56:54 +0300

    Remastered

--- src/test/java/utilities/BrowserFactory.java
+++ src/test/java/utilities/BrowserFactory.java
@@ -4,17 +4,12 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.safari.SafariDriver;
 
 public class BrowserFactory {
-
     private static WebDriver driver;
-
     public static WebDriver getDriver(String browser) {
         // Test
         if (driver == null) {
@@ -46,12 +41,9 @@ public class BrowserFactory {
                     driver = new SafariDriver();
                     break;
             }
-
         }
-
         return driver;
     }
-
     public static void closeDriver() {
         if (driver != null) {
             driver.quit();
